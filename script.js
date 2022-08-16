c42a2d8bc66dceb450b8222105e21471cf56384e
Ivan <ivanlisichkin@icloud.com>
2022-08-16 22:28:52 +0300

    feat: add task

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-function countPositivesSumNegatives(input) {
-  let positives = 0;
-  let negative = 0;
-  if(input == null || input.length < 1){
-    return []
-  }
-  else{
-    input.map((count)=> count > 0 ? positives++ : negative+=count)
-    return [positives, negative]
-  }
+function lovefunc(flower1, flower2){
+  return (flower1 % 2 === 0 || flower2 % 2 === 0) && (flower1 % 2 !== 0 || flower2 % 2 !== 0) ? true : false
 }
-countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])//?
+lovefunc(1,4)//?
