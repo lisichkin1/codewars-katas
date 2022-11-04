f0ed38238b7cee2f2a47c11fadfb95d08ff5b3a0
Ivan <ivanlisichkin@icloud.com>
2022-11-04 13:29:18 +0300

    feat: add 8kyu task

--- script.js
+++
@@ -1,5 +0,0 @@
-function isPythagoreanTriple(integers) {
-  let sortArr = integers.sort((a, b) => a - b)
-  return sortArr[0]**2 + sortArr[1]**2 == sortArr[2]**2 ? true : false;
-}
-isPythagoreanTriple([15275, 12220, 9165])//?
