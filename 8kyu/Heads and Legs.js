0a00b8aeb695f708a94593fb5f3c0370f664e386
Ivan <ivanlisichkin@icloud.com>
2022-11-04 13:22:31 +0300

    feat: add 8kyu task

--- 8kyu/Heads and Legs.js
+++
@@ -1,9 +0,0 @@
-function animals(heads, legs){
-    let chickenLegs = heads * 2
-    let differenceLegs = legs - chickenLegs
-    let cowsRes = differenceLegs/2//?
-    let ChickenLegsRes = heads - cowsRes//?
-    return heads == 0 && legs == 0 ? [0,0]:
-            heads == 0 || legs == 0 || legs % 2 != 0 || ChickenLegsRes < 0 || cowsRes < 0? 
-            "No solutions" : [ChickenLegsRes,cowsRes]
-  }
---
+++ 8kyu/Pythagorean Triple.js
@@ -0,0 +1,4 @@
+function isPythagoreanTriple(integers) {
+    let sortArr = integers.sort((a, b) => a - b)
+    return sortArr[0]**2 + sortArr[1]**2 == sortArr[2]**2 ? true : false;
+  }
