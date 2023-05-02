22bdcb1baa20c7ffc0b65d4c51991a08b605210c
Ivan <ivanlisichkin@icloud.com>
2023-05-02 15:13:03 +0400

    feat: add new task

---
+++ script.js
@@ -0,0 +1,7 @@
+function dontGiveMeFive(start, end){
+    let result = [];
+    for (let i = start; i <= end; i++) {
+      if (!i.toString().includes('5')) result.push(i);
+    }
+    return result.length;//?
+  }
